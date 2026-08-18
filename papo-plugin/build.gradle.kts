7fd9f435ad9e3d7dc3546df324c93cdf3aec9b55
Zurker <91011862+hershate@users.noreply.github.com>
2026-08-18 19:44:31 +0800

    chore(papo-plugin): release 1.0.1 - version bump forces pack reinstall + auto RP regeneration on all existing deployments

--- papo-plugin/build.gradle.kts
+++ papo-plugin/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "com.nhoryzon.mc"
-version = "1.0.0"
+version = "1.0.1"
 description = "Farmer's Delight rewritten for Papo (Paper 1.21.11) with CraftEngine-hosted assets"
 
 repositories {
